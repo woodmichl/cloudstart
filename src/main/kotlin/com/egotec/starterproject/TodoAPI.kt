6da445a637fd1025c76e2ceda1df65791e12af91
Michael <michael@hahn9-dorf.de>
2022-11-07 14:09:25 +0100

    added CRUD operations to API and made Environment Windows compatible

--- src/main/kotlin/com/egotec/starterproject/TodoAPI.kt
+++ src/main/kotlin/com/egotec/starterproject/TodoAPI.kt
@@ -13,7 +13,8 @@ class TodoAPI {
     @Produces(MediaType.APPLICATION_JSON)
     fun getTodo(@PathParam("id") id: String): TodoEntity {
         val state = ThreadState.begin()
-        return state.em.find(TodoEntity::class.java, id) ?: throw WebApplicationException(Response.Status.NOT_FOUND)
+        return state.em.find(TodoEntity::class.java, id.toLong())
+            ?: throw WebApplicationException(Response.Status.NOT_FOUND)
     }
 
     @POST
@@ -22,14 +23,51 @@ class TodoAPI {
     fun createTodo(todoEntity: TodoEntity): TodoEntity {
         val state = ThreadState.begin()
         state.em.transaction.begin()
-        state.em.merge(todoEntity)
+        state.em.persist(todoEntity)
         state.em.transaction.commit()
         return todoEntity
     }
 
     // TODO:
     // getAllTodos
-    // deleteTodo
-    // updateTodo
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getTodoList(): List<TodoEntity> {
+        val state = ThreadState.begin()
+        return state.em.createQuery("select a from TodoEntity a", TodoEntity::class.java).resultList
+    }
 
+    // deleteTodo
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun deleteTodo(@PathParam("id") id: String): Boolean {
+        val state = ThreadState.begin()
+        val toRemove = state.em.find(TodoEntity::class.java, id.toLong())
+            ?: throw WebApplicationException(Response.Status.NOT_FOUND)
+        state.em.transaction.begin()
+        state.em.remove(toRemove)
+        state.em.transaction.commit()
+        return true
+    }
+
+    // updateTodo
+    @PATCH
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    fun updateTodo(@PathParam("id") id: String, todoEntity: TodoEntity?): Boolean {
+        if (todoEntity == null) {
+            throw WebApplicationException(Response.Status.BAD_REQUEST)
+        }
+        val state = ThreadState.begin()
+        val toUpdate = state.em.find(TodoEntity::class.java, id.toLong())
+            ?: throw WebApplicationException(Response.Status.NOT_FOUND)
+        state.em.transaction.begin()
+        toUpdate.content = todoEntity.content ?: toUpdate.content
+        toUpdate.name = todoEntity.name ?: toUpdate.name
+        toUpdate.done = todoEntity.done
+        state.em.transaction.commit()
+        return true
+    }
 }
