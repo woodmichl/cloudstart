403cf3a22e2e0ec904396104ca829a3660e21b7c
Michael <michael@hahn9-dorf.de>
2022-11-08 13:25:26 +0100

    re-implemented frontend, changed README.md

--- frontend/src/app/list/list.component.ts
+++ frontend/src/app/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Listitem } from "../listitem";
-import { ListitemService} from "../listitem.service";
+import {Component, OnInit} from '@angular/core';
+import {ListitemService} from "../listitem.service";
+import {Listitem} from "../listitem";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-list',
@@ -8,23 +9,42 @@ import { ListitemService} from "../listitem.service";
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  devMode = false;
+
+  addedItem: Listitem = {id: 0, name: "", content: "", done: false}
 
   listitems: Listitem[] = [];
 
-  constructor(private listService: ListitemService) { }
+  constructor(private listService: ListitemService,
+              private http: HttpClient) {
+  }
 
   ngOnInit(): void {
     this.getListitems();
   }
+
 
   getListitems(): void {
     this.listService.getListitems()
       .subscribe(listitems => this.listitems = listitems);
   }
 
-  delete(listitem: Listitem):void{
+  delete(listitem: Listitem): void {
+    this.http.delete("/api/todo/"+listitem.id).subscribe(()=>this.getListitems())
     //delete from Database
   }
+
+  logItems(){
+    console.log(this.listitems)
+  }
+
+  updateItem(changedItem: Listitem) {
+    this.http.patch("/api/todo/"+changedItem.id,changedItem).subscribe(()=> {
+      this.getListitems()
+    })
+  }
+  createTodo() {
+    this.http.post<Listitem>("/api/todo",this.addedItem).subscribe(()=>this.getListitems())
+    this.addedItem = {id: 0, name: "", content: "", done: false}
+  }
 }
-
-
